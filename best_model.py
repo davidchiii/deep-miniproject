36b944ea70f070d476f1b7c2949d7b937b9ac324
Jimmy Shong <jimmysh341@gmail.com>
2024-04-13 02:47:56 -0400

    Add comments

--- best_model.py
+++ best_model.py
@@ -14,21 +14,31 @@ import argparse
 from models.dropoutresnet import DropoutResNet18
 
 
-# Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
+    
+    # switch to train mode
     net.train()
     train_loss = 0
     correct = 0
     total = 0
+    
+    # start training epoch
     for batch_idx, (inputs, targets) in enumerate(trainloader):
+        
+        # move to device
         inputs, targets = inputs.to(device), targets.to(device)
+       
+        # forward pass
         optimizer.zero_grad()
         outputs = net(inputs)
+        
+        # backpropogate
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
 
+        # calculate loss and accuracy
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
@@ -39,16 +49,23 @@ def train(epoch):
 
 def test(epoch):
     global best_acc
+
+    # switch to eval mode
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
+        
+            # start test epoch
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
 
+            # forward pass
+            outputs = net(inputs)
+            
+            # calculate loss and accuracy
+            loss = criterion(outputs, targets)
             test_loss += loss.item()
             _, predicted = outputs.max(1)
             total += targets.size(0)
@@ -58,6 +75,8 @@ def test(epoch):
 
     # Save checkpoint.
     acc = 100.*correct/total
+    
+    # Save best epoch checkpoint
     if acc > best_acc:
         print('Saving..')
         state = {
@@ -69,6 +88,8 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/best_epoch.pth')
         best_acc = acc
+    
+    # Save model
     if epoch == 199:
         state = {
             'net': net.state_dict(),
@@ -80,18 +101,21 @@ def test(epoch):
 
 
 if __name__ == '__main__':
+    # Parse arguments
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
     parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
     parser.add_argument('--resume', '-r', action='store_true',
                         help='resume from checkpoint')
     args = parser.parse_args()
 
+    # Load variabels for training
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     best_acc = 0  # best test accuracy
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-    # Data
     print('==> Preparing data..')
+    
+    # Data augmentation and normalization for training
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -101,27 +125,34 @@ if __name__ == '__main__':
     transform_test = transforms.Compose([
         transforms.ToTensor(),
     ])
+    
+    # Create dataloaders
     trainset = torchvision.datasets.CIFAR10(
         root='./deep-learning-mini-project-spring-24-nyu/cifar-10-python', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=128, shuffle=True, num_workers=2)
 
+    # Load dataset with tranformation
     testset = torchvision.datasets.CIFAR10(
         root='./deep-learning-mini-project-spring-24-nyu/cifar-10-python', train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=100, shuffle=False, num_workers=2)
 
+    # List of classes
     classes = ('plane', 'car', 'bird', 'cat', 'deer',
             'dog', 'frog', 'horse', 'ship', 'truck')
 
-    # Model
     print('==> Building model..')
+    # Initialize the model
     net = DropoutResNet18(0.4)
     net = net.to(device)
+    
+    # If the model is on cuda, use DataParallel
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
 
+    # Load command line arguments
     if args.resume:
         # Load checkpoint.
         print('==> Resuming from checkpoint..')
@@ -130,13 +161,17 @@ if __name__ == '__main__':
         net.load_state_dict(checkpoint['net'])
         best_acc = checkpoint['acc']
         start_epoch = checkpoint['epoch']
-
+        
+    # Define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr,
                         momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+    # Print out the model parameters
     summary(net, (3,32,32))
 
+    # Running Training and Inference
     for epoch in range(start_epoch, 200):
         train(epoch)
         test(epoch)
--- official_baseline.py
+++ official_baseline.py
@@ -17,18 +17,28 @@ from models.modifiedresnet import ModifiedResNet18
 # Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
+    
+    # switch to train mode
     net.train()
     train_loss = 0
     correct = 0
     total = 0
+    
+    # start training epoch
     for batch_idx, (inputs, targets) in enumerate(trainloader):
+        # move to device
         inputs, targets = inputs.to(device), targets.to(device)
+        
+        # forward pass
         optimizer.zero_grad()
         outputs = net(inputs)
+       
+        # backpropogate
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
-
+        
+        # calculate loss and accuracy
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
@@ -40,16 +50,23 @@ def train(epoch):
 
 def test(epoch):
     global best_acc
+    
+    # switch to eval mode
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
+            
+            # start test epoch
             inputs, targets = inputs.to(device), targets.to(device)
+            
+            # forward pass
             outputs = net(inputs)
+            
+            # calculate loss and accuracy
             loss = criterion(outputs, targets)
-
             test_loss += loss.item()
             _, predicted = outputs.max(1)
             total += targets.size(0)
@@ -60,6 +77,8 @@ def test(epoch):
 
     # Save checkpoint.
     acc = 100.*correct/total
+    
+    # Save best epoch checkpoint
     if acc > best_acc:
         print('Saving..')
         state = {
@@ -73,18 +92,22 @@ def test(epoch):
         best_acc = acc
 
 if __name__ == '__main__':
+    # Parse arguments
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
     parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
     parser.add_argument('--resume', '-r', action='store_true',
                         help='resume from checkpoint')
     args = parser.parse_args()
-
+    
+    # Load variabels for training
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     best_acc = 0  # best test accuracy
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
     # Data
     print('==> Preparing data..')
+    
+    # Data augmentation and normalization for training
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -96,42 +119,34 @@ if __name__ == '__main__':
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
+    
+    # Create dataloaders
     trainset = torchvision.datasets.CIFAR10(
         root='./deep-learning-mini-project-spring-24-nyu/cifar-10-python', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=128, shuffle=True, num_workers=2)
-
+    
+    # Load dataset with tranformation
     testset = torchvision.datasets.CIFAR10(
         root='./deep-learning-mini-project-spring-24-nyu/cifar-10-python', train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=100, shuffle=False, num_workers=2)
 
+    # List of classes
     classes = ('plane', 'car', 'bird', 'cat', 'deer',
             'dog', 'frog', 'horse', 'ship', 'truck')
 
-    # Model
-    print('==> Building model..')
-    # net = VGG('VGG19')
-    # net = ResNet18()
+    print('==> Building model..')    
+    # Initialize the model
     net = ModifiedResNet18()
-    # net = PreActResNet18()
-    # net = GoogLeNet()
-    # net = DenseNet121()
-    # net = ResNeXt29_2x64d()
-    # net = MobileNet()
-    # net = MobileNetV2()
-    # net = DPN92()
-    # net = ShuffleNetG2()
-    # net = SENet18()
-    # net = ShuffleNetV2(1)
-    # net = EfficientNetB0()
-    # net = RegNetX_200MF()
-    # net = SimpleDLA()
     net = net.to(device)
+
+    # If the model is on cuda, use DataParallel
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
 
+    # Load command line arguments
     if args.resume:
         # Load checkpoint.
         print('==> Resuming from checkpoint..')
@@ -141,12 +156,16 @@ if __name__ == '__main__':
         best_acc = checkpoint['acc']
         start_epoch = checkpoint['epoch']
 
+    # Define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr,
                         momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+    # Print out the model parameters
     summary(net, (3,32,32))
 
+    # Running Training and Inference
     for epoch in range(start_epoch, start_epoch+200):
         train(epoch)
         test(epoch)
