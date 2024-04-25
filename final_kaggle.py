7550554ca71663664d0e2d4b82f75d1e62d18290
Jimmy Shong <js11718@gv002.hpc.nyu.edu>
2024-04-25 05:18:51 -0400

    changed notebook

--- final_kaggle.py
+++ final_kaggle.py
@@ -20,6 +20,7 @@ def load_cifar_batch(file):
 #  Converting to tensor
 test_transform = transforms.Compose([
     transforms.ToTensor(),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 # Load and preprocess kaggle dataset
