ee8d8f945ca1657d965a367bdf1c7e5d794c3a24
Jimmy Shong <js11718@gv002.hpc.nyu.edu>
2024-04-25 05:24:48 -0400

    fixed kaggle generator scripts

--- kaggle.py
+++ kaggle.py
@@ -9,8 +9,6 @@ import torch
 import torchvision
 import torch.nn.parallel
 import torchvision.transforms as transforms
-
-
 from collections import OrderedDict
 
 # Function for loading Kaggle data
@@ -19,43 +17,35 @@ def load_cifar_batch(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-#  Color augmentation if needed
-transform_test = transforms.Compose([
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+#  Converting to tensor
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 # Load and preprocess kaggle dataset
 cifar10_batch = load_cifar_batch('./deep-learning-mini-project-spring-24-nyu/cifar_test_nolabels.pkl')
-images = torch.from_numpy(cifar10_batch[b'data'])
-images = images.reshape((10000, 3, 32, 32))
-images = images.float()
-# images = transform_test(images)
+images = cifar10_batch[b'data'].reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1) 
 
 # Assign device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load Model
 net = DropoutResNet18(0.4)
-# net = ModifiedResNet18()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
 checkpoint = torch.load('./checkpoint/best_epoch_3.pth', map_location=device)
-# print(checkpoint['acc'])
-# checkpoint = torch.load('./checkpoint/ckpt_dropout_0.4.pth', map_location=device)
-# checkpoint = torch.load('./checkpoint/pool.pth', map_location=device)
-
 
 # Run inference on Kaggle Set
 net.load_state_dict(checkpoint['net'])
+net.eval()
 with torch.no_grad():
-    images = images.to(device)
-    output = net(images)
-    _, predicted = output.max(1)
-    predicted = predicted.tolist()
-    for i in predicted:
-        print(i)
-    
+    for i in range(10000):
+        image = images[i]
+        input = torch.unsqueeze(test_transform(image),0)
+        output = net(input)
+        _, predicted = output.max(1)
+        print(predicted.item())
